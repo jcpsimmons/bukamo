976741f839d1a2c03cb2da3be1a0b6ad82ff7ea1
josh simmons <joshcsimmons@hey.com>
2024-07-14 19:37:00 -0700

    packages init

--- packages/common.nix
+++ packages/common.nix
@@ -2,78 +2,76 @@
 
 {
   home.packages = with pkgs; [
-    kjv
-
-    # GUI Applications
-    cool-retro-term
-    vscode
-    obsidian
-    wezterm
-
-    # Shell
-    neovim
-    tmux
-    zsh
-    oh-my-zsh
-    zsh-autocomplete
-    zsh-autosuggestions
-    zsh-syntax-highlighting
-    tldr
-    jq
-    devenv
-
-    # File Ops Tools
-    tree
+    # from terminal script
+    curl
     git
     unzip
-    ripgrep
-    fzf
-    bat
-    entr
-    duf
 
-    # Process Handling
-    btop
-    killall
-
-    # Networking
-    gping
-    speedtest-cli
-    dog
-    ngrok
-
-    # Lang Specific
-    go
-    bun
-    deno
-    cargo
-    pipx
-    python3
-    nodePackages_latest.typescript-language-server
-    vscode-langservers-extracted
-    emmet-language-server
-    tailwindcss-language-server
-
-    # Networking
-    lsof
-    nmap
-
-    # Nix-Related
-    nix-prefetch-git
-    nil
-    nixd
-    nixpkgs-fmt
-
-    # Media
-    spotify-player
-    yt-dlp
-
-    # Misc
-    ponysay
+    # from terminal sub-scripts
+    mise
+    gpg
     gh
-    typst
-    (nerdfonts.override { fonts = [ "BigBlueTerminal" "JetBrainsMono" ]; })
-    dosbox-staging
-    newsboat
+    fastfetch
+    fzf
+    ripgrep
+    bat
+    eza
+    zoxide
+    plocate
+    btop
+    apache2-utils
+    fd-find
+    tldr
+    build-essential
+    pkg-config
+    autoconf
+    bison
+    clang
+    libssl-dev
+    libreadline-dev
+    zlib1g-dev
+    libyaml-dev
+    libreadline-dev
+    libncurses5-dev
+    libffi-dev
+    libgdbm-dev
+    libjemalloc2
+    libvips
+    imagemagick
+    libmagickwand-dev
+    mupdf
+    mupdf-tools
+    redis-tools
+    sqlite3
+    libsqlite3-0
+    libmysqlclient-dev
+    docker
+    docker-buildx
+
+    # from desktop script
+    typora
+    xournalpp
+    localsend
+    vlc
+    google-chrome
+    alacritty
+    steam
+    virtualbox
+    virtualboxExtpack
+    zoom
+    dropbox
+    spotify
+    brave
+    _1password
+    _1password-gui
+    emacs
+    obs-studio
+    flameshot
+    ulauncher
+    flatpak
+    gnomeExtensions.pano
+    signal-desktop
+    vscode
+    gnome.sushi
   ];
 }
